bad1e889a71abb4d07d37e9160db328ac5f86fc6
adityadam2005 <aditya.animationmedia@gmail.com>
2025-11-10 00:45:09 +0530

    Updated Padding

--- app/(app)/home.tsx
+++ app/(app)/home.tsx
@@ -172,7 +172,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between',
     alignItems: 'center',
     paddingHorizontal: 22,
-    paddingTop: 48,
+    paddingTop: 20,
     paddingBottom: 10,
   },
 
